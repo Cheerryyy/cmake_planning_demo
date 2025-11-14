7bae91d6eced40740ef87fc0f65054b6627d904b
chency <chenchunyu1225@gmail.com>
2025-11-14 17:08:27 +0800

    add third_party

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,10 +1,21 @@
-#include<iostream>
+#include <iostream>
 #include "process.h"
+#include <Eigen/Dense>
 using std::cout, std::endl;
+using Eigen::MatrixXd;
 
 void Process::planProcess()
 {
     cout << "this is planing process" << endl;
     my_map.mapInfo();
+
+    MatrixXd mat(2,2);
+    mat(0,0) = 3;
+    mat(0,1) = 2.5;
+    mat(1,0) = -1;
+    mat(1,1) = mat(0,1) + mat(1,0);
+    cout << mat << endl;
+
     cout << "planning success!" << endl;
 } 
+
