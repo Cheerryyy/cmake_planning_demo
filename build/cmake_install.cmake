8f41c36544ef248aad30feaa0ab7bb5e87da21f3
chency <chenchunyu1225@gmail.com>
2025-11-14 15:16:22 +0800

    version 0.0.1

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Users/ChenC/Software/vscode_cpp_tools/mingw64/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/ChenC/CppCode/cmake_planning_demo/build/src/cmake_install.cmake")
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
